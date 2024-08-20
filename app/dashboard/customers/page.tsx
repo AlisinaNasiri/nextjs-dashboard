f70553b8f95baf85036e7d3d5d77aaf72db82288
AlisinaNasiri <alisinanasiri97@gmail.com>
2024-08-20 20:43:15 +0430

    Finished v0.0.1

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customer",
+};
 export default function Page() {
   return <p>Welcome to the Customers Page</p>;
 }
